d3ea97fcad044be5972f1da31c08531fe3baf3ab
cerry24 <samuelecerretti@gmail.com>
2022-12-05 16:42:10 +0100

    implement array members

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const members = [
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        photo : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name : 'Angela caroll',
+        role : 'Chief Editor',
+        photo : 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        photo : 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
+        photo : 'Manager  angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name : 'Scott Estrada',
+        role : 'Developer',
+        photo : 'scott-estrada-developer.jpg'
+    },
+    {
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        photo : 'barbara-ramos-graphic-designer.jpg'
+    },
+];
